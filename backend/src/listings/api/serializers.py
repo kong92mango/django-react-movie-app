486489276e4e5939ce99a3b30877ac6559a5eb08
Kong Cheng <kongcheng18@gmail.com>
2020-04-19 18:48:29 -0700

    Added serializer for all models and display all information on frontend

--- backend/src/listings/api/serializers.py
+++ backend/src/listings/api/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 
 from listings.models import Listing
+from listings.models import Schedule
+from listings.models import Genre
+
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Schedule
+        fields = '__all__'  
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'  
 
 class ListingSerializer(serializers.ModelSerializer):
+    schedule = ScheduleSerializer(read_only=True, many = True)
+    genre = GenreSerializer(read_only=True, many = True)
+
     class Meta:
         model = Listing
-        fields = '__all__'
+        fields = '__all__'      
